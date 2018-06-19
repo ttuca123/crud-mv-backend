b81a04e41bfcbf66eece35fef0e120323c0fdde4
DESKTOP-25GTG0A\Artur <ttuca123@gmail.com>
2018-06-19 06:53:53 -0300

    Criação do pacote config

--- src/main/java/br/com/mv/config/WebConfig.java
+++ src/main/java/br/com/mv/config/WebConfig.java
@@ -1,4 +1,4 @@
-package br.com.mv.utils;
+package br.com.mv.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
